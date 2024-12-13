b5981842d7e6d408e105ddb34769b21c08d39b73
veyhn <vennisabarfi@gmail.com>
2024-12-13 16:34:52 -0500

    header and tab section fixed now off to about section.

---
+++ frontend/src/Header/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css"
+
+export default function Header(){
+
+    return(
+        <>
+        <div className="header-animation">
+        <h1 className="scroll-m-20  font-extrabold tracking-tight lg:text-5xl">
+      </h1>
+        </div>
+        
+        </>
+    );
+}
