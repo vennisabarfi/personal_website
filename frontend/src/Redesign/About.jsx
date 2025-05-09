19a3c9193401cfc1aec139582cf10f2d528f9065
unknown <vennisabarfi@gmail.com>
2025-05-08 23:07:30 -0400

    working on about age. and cleaning up on classnames for p tags

--- frontend/src/Redesign/About.jsx
+++ frontend/src/Redesign/About.jsx
@@ -1,11 +1,21 @@
 
-
+import "../Redesign/About.css"
 export default function About(){
 
     return(
         <>
-        <div className="intro"></div>
-        <h2 className="text-4xl font-bold">About</h2>
+        <div className="intro">
+
+        <h1 className="text-4xl font-bold mt-14">About</h1>
+        <p>Welcome to my tiny little space on the internet!  </p>
+
+        <p>I love to build software and work with data and I&apos;ve been spending time working on projects, building my skills and working in the freelance space.</p>
+
+        <p>Here are the projects I have been working on:
+            <ul>Astro</ul>
+        </p>
+        </div>
+    
         </>
     );
 }
