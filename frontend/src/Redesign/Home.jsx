e92c4b697fe8556bf06a594cad6830c5d34299f8
unknown <vennisabarfi@gmail.com>
2025-05-08 20:52:44 -0400

    styled home page and added links and color effects

--- frontend/src/Redesign/Home.jsx
+++ frontend/src/Redesign/Home.jsx
@@ -1,15 +1,21 @@
 import "./Home.css"
 
 
+
 export default function Home(){
 
     return(
         <>
         <div className="center-intro">
-        <ul> Hello </ul>
-        <ul>My Name is Vennisa</ul>
-        <ul> Blog (Building...)</ul>
-        <ul>About</ul>
+        <ul className="text-6xl font-bold"> Vennisa Barfi </ul>
+        <ul className="text-3xl">Something something</ul>
+
+        <ul className="link text-2xl"> 
+            <a href="/blog">Blog (Building...)</a>
+        </ul>
+        <ul className="link text-2xl">
+            <a href="/about">About</a>
+            </ul>
 
         </div>
         </>
