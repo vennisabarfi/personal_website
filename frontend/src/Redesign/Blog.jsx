496b09591795514d8d7f694f311f8aed6fba260a
unknown <vennisabarfi@gmail.com>
2025-05-09 00:44:18 -0400

    worked on styling for mobile screens yayy. will work on blog design tomorrow. pull request incoming

--- frontend/src/Redesign/Blog.jsx
+++ frontend/src/Redesign/Blog.jsx
@@ -4,6 +4,7 @@ export default function Blog(){
 
     return(
         <>
+        <div className="center-intro text-2xl"> In progress...Will start posting soon...</div>
         </>
     );
 }
--- frontend/src/Redesign/Home.jsx
+++ frontend/src/Redesign/Home.jsx
@@ -9,14 +9,15 @@ export default function Home(){
         <>
         <div className="center-intro">
         <ul className="text-6xl font-bold"> Vennisa Barfi </ul>
-        <ul className="text-3xl">Something something</ul>
+        {/* <ul className="text-3xl">Something something</ul> */}
         <br/>
-        <ul className="link text-2xl"> 
-            <a href="/blog">Blog (Building...)</a>
-        </ul>
         <ul className="link text-2xl">
             <a href="/about">About</a>
             </ul>
+        <ul className="link text-2xl"> 
+            <a href="/blog">Blog (Building...)</a>
+        </ul>
+        
 
         </div>
         </>
