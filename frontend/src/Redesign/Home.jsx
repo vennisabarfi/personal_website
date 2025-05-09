a1784de895bacb49492430e280d1f5413c5967b0
unknown <vennisabarfi@gmail.com>
2025-05-08 20:17:17 -0400

    redesigning home page. cooking

---
+++ frontend/src/Redesign/Home.jsx
@@ -0,0 +1,17 @@
+import "./Home.css"
+
+
+export default function Home(){
+
+    return(
+        <>
+        <div className="center-intro">
+        <ul> Hello </ul>
+        <ul>My Name is Vennisa</ul>
+        <ul> Blog (Building...)</ul>
+        <ul>About</ul>
+
+        </div>
+        </>
+    );
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Projects from './Projects/Projects'
-import Resume from './About/Resume'
+import Home from './Redesign/Home'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
     <BrowserRouter>
     <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/projects" element={<Projects />} />
-    <Route path="/resume" element={<Resume />} />
+    <Route path="/" element={<Home />} />
+   
     </Routes>
     </BrowserRouter>
   </StrictMode>,
